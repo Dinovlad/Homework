bf87a2b4b77da1a35c8b625a009b30e5f72ed95c
Dinovlad <vladislavkozhevnikov@gmail.com>
2016-11-23 23:14:57 +0300

    Add files via upload

--- Chat/serv.c
+++ Chat/serv.c
@@ -10,7 +10,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-
+#include <time.h>
 
 #define check() errCheck(__LINE__)
 
@@ -24,47 +24,150 @@ void errCheck(int line) {
 	}
 }
 
+#define debug() dbg(__LINE__)
+
+void dbg(int line) {
+	printf("The line %d passed.\n", line);
+}
+
 #define limit 50
 
-struct clientInf { // client information
+// clients list
+
+#define nameLength 100
+
+struct clientInfo { // client information
 
 	int fd; // socket file descriptor
 
-	char name[100];
+	char name[nameLength + 1];
 
 };
 
-
-struct clientInf **cls; // clients
+struct clientInfo *heap; // heap of empty structures
+struct clientInfo **cls; // clients
 int *clN; // clients number
 
-void addClient(struct clientInf *cl) {
+void addClient(struct clientInfo *cl) {
 
-	cls[clN] = cl;
+	cls[*clN] = cl;
 
-	clN++;
+	clN[0]++;
 
 }
 
-void removeClient(struct clientInf *cl) {
+void removeClient(struct clientInfo *cl) {
 
 	int i;
 
-	for(i = 0; i < clN; i++) {
+	for(i = 0; i < clN[0]; i++) {
 		if(cls[i] == cl) {
 			break;
 		}
 	}
 
-	for(i = i + 1; i < clN; i++) {
+	for(i = i + 1; i < clN[0]; i++) {
 		cls[i - 1] = cls[i];
 	}
 
-	clN--;
+	clN[0]--;
 
 }
 
+// clients list end
 
+// semaphore
+
+int semid;
+
+struct sembuf sbuf[2];
+
+// blocks the clients array or suspends the execution
+// of the calling process if it is already blocked
+void block() { 
+
+	semop(semid, &sbuf[1], 1); 
+	check();
+
+}
+
+void unblock() { // unblocks the clients array
+
+        semop(semid, sbuf, 1); 
+        check();
+
+}
+
+// semaphore end
+
+// communnication implements
+
+// the procedure does not return until count bytes are read
+void readFull(int fd, void *buf, size_t count) {
+
+	size_t n;
+
+	while(count) {
+
+		n = read(fd, buf, count);
+		check();
+
+		buf = (void *) (buf + n);
+
+		count -= n;
+
+	}
+
+}
+
+// the function reads the next message and
+// returns the number of bytes read and written to buf
+int readMessage(int socketfd, void *buf, int bufsize) {
+
+	int size;
+
+	readFull(socketfd, &size, 4); // read the size of the message
+
+	if (bufsize < size) {
+
+		printf("The capacity of the buffer is not sufficient.\n");
+
+		exit(-1);		
+
+	}
+
+	readFull(socketfd, buf, size);
+
+	return size;
+
+}
+
+void writeFull(int fd, void *buf, size_t count) {
+
+	size_t n;
+
+	while(count) {
+
+		n = write(fd, buf, count);
+		check();
+
+		buf = (void *) (buf + n);
+
+		count -= n;
+
+	}
+
+}
+
+void writeMessage(int socketfd, void *buf, int count) {
+
+	writeFull(socketfd, &count, 4);
+
+	writeFull(socketfd, buf, count);
+
+}
+
+// communication implements end
 
 int main() {
 
@@ -75,11 +178,11 @@ int main() {
 
 	struct sockaddr_in addr;
 
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        serv_addr.sin_port = htons(5000);
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = htons(5000);
 
-	bind(sfd, &addr, sizeof(struct sockaddr_in));
+	bind(sfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
 	check();
 
 	listen(sfd, 10);
@@ -91,22 +194,26 @@ int main() {
 
 	clN = mmap(NULL, 4 + limit * sizeof(size_t) + limit * sizeof(struct clientInfo), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0); // clients number
 	// heap of clients information structures
-	struct clientInfo *heap = clN + 4 + limit * sizeof(size_t);
-	// the unused space must be set to zero 
-	memset(heap, 0, limit * sizeof(struct clientInfo));
-	cls =  clN + 4; // array of *clients information
+	heap = (struct clientInfo *) (clN + 4 + limit * sizeof(size_t));
+	// the unused space must be set to zero
+	memset(heap, 0, limit * sizeof(struct clientInfo)); // segm. fault
+
+	cls =  (struct clientInfo **) (clN + 4); // array of *clients information
 	clN[0] = 0;
 
 	// the clients list created
 
 	// 3. create a semaphore for parallel work with the clients list
 
-	int semid = semget(ftok("serv.c"), 1, IPC_CREAT);
+	semid = semget(ftok("serv.c", 0), 1, 0666 | IPC_CREAT);
 	check();
 
-	struct sembuf sbuf; //semaphore buffer
-	sbuf.sem_num = 0;
-	sbuf.sem_flg = 0;
+	sbuf[0].sem_num = 0;
+	sbuf[0].sem_flg = 0;
+	sbuf[0].sem_op = 1;
+
+	sbuf[1] = sbuf[0];
+	sbuf[1].sem_op = -1;
 
 	// set the initial value == 1
 
@@ -131,10 +238,12 @@ int main() {
 
 	while(1) {
 
+		printf("The server has successfully started.\n");
+
 		cfd = accept(sfd, NULL, NULL);
 		check();
 
-		pid = fork();
+		int pid = fork();
 
 		if (pid == 0) { // the child must leave the cycle
 			break; 
@@ -144,21 +253,30 @@ int main() {
 
 	}
 
-	#define bufsize 1000
+	#define mesize 1000 // message size
 
-	char buf[bufsize + 1];
+	struct buffer {
+
+		char name[nameLength];
+
+		time_t time;
+
+		char message[mesize];
+
+	} buf;
+
 	int l; // length (of the string read)
 
 	// fisrt, register the client
 	// the first message is treated as the name
 
-	l = read(client.fd, buf, bufsize);
+	l = readMessage(cfd, buf.message, mesize);
+	if (l > nameLength) {
+		l = nameLength;
+	}
+	buf.message[l - 1] = 0;
 
 	// start initialisation
-
-	// declare the semaphore functions
-	void block();
-	void unblock();
 
 	block(); 
 
@@ -170,11 +288,9 @@ int main() {
 	struct clientInfo *client = &heap[i];
 
 	client[0].fd = cfd;
+	sprintf(client[0].name, "%s", buf.message); 
 
-	buf[l] = 0;
-	sprintf(client[0].name, "%s", buf); 
-
-	addClient(client) //add the *structure to the clients list
+	addClient(client); //add the *structure to the clients list
 
 	unblock();
 
@@ -184,40 +300,25 @@ int main() {
 
 	while(1) {
 
-		l = read(client[0].fd, buf, bufsize);
+		l = readMessage(client[0].fd, buf.message, mesize);
+		buf.message[l - 1] = 0;
+
+		buf.time = time(NULL);
 
 		block();
 
-		for(i = 0; i < clN; i++) {
+		for(i = 0; i < clN[0]; i++) {
 	
 			if (cls[i] == client) {
 				continue;
 			}
 
-			write(cls[i][0].fd, buf, l);
+			writeMessage(cls[i][0].fd, &buf, sizeof(struct buffer));
 
 		}
 
 		unblock();
 
 	}
-
-}
-
-// blocks the clients array or suspends the execution
-// of the calling process if it is already blocked
-void block() { 
-
-        sbuf.sem_op = -1;
-        semop(semid, &sbuf, 1); 
-        check();
-
-}
-
-void unblock() { // unblocks the clients array
-
-        sbuf.sem_op = 1;
-        semop(semid, &sbuf, 1); 
-        check();
 
 }
