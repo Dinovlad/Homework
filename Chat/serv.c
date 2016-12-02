2968adfd0669d889ffac1ed850b9893aa028bccd
Dinovlad <vladislavkozhevnikov@gmail.com>
2016-12-02 22:06:34 +0300

    Add files via upload

--- Chat/serv.c
+++ Chat/serv.c
@@ -12,6 +12,10 @@
 #include <signal.h>
 #include <pthread.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
 #define check() errCheck(__LINE__)
 
 void errCheck(int line) {
@@ -32,15 +36,20 @@ void dbg(int line) {
 
 #define limit 50
 
-// clients list
+// active clients list
 
 #define nameLength 100
+#define passLength 50
 
 struct clientInfo { // client information
+
+	char isActive;
 
 	pthread_t thr; // thread (id)
 
 	int fd; // socket file descriptor
+
+	//char password[passLength + 1];
 
 	char name[nameLength + 1];
 
@@ -68,14 +77,76 @@ void removeClient(int i) {
 
 }
 
-// clients list end
+pthread_mutex_t lmutex;
+
+// active clients list end
+
+// clients register
+
+char reg[limit * (4 + nameLength + 1 + passLength + 1)];
+int regSize;
+
+int regSeek(char *name) {
+
+	int i = 0;
+
+	while(i < regSize)  {
+
+		if (strcmp(&reg[i + 4], name) == 0) {
+			
+			i += 4;
+			while (reg[i]) {
+				i++;
+			}
+
+			return i + 1;
+
+		}
+
+		i += *((int *) &reg[i]) + 4;
+
+	}
+
+	return -1;
+
+}
+
+void regAdd(char *name, char *buf, int bufSize) {
+
+	int l = strlen(name) + 1; // + 1 accounting for 0 byte
+
+	char *pos = &reg[regSize];
+
+	*((int *) pos) = bufSize + l;
+
+	pos = &pos[4];
+
+	sprintf(pos, "%s", name);
+
+	pos = &pos[l];
+
+	int i = 0;
+
+	while(i < bufSize) {
+		
+		pos[i] = buf[i];
+
+		i++;
+
+	}
+
+	regSize += 4 + bufSize + l;
+
+}
+
+pthread_mutex_t rmutex;
+
+// clients register end
 
 // mutex
 
-pthread_mutex_t mutex;
-
-#define block() pthread_mutex_lock(&mutex)
-#define unblock() pthread_mutex_unlock(&mutex)
+#define block(mutex) pthread_mutex_lock(mutex)
+#define unblock(mutex) pthread_mutex_unlock(mutex)
 
 // mutex end
 
@@ -97,12 +168,12 @@ void broadcast(void *buf, size_t count, struct clientInfo *source) {
 
 	for(i = 0; i < clN; i++) {
 
-		if (cls[i] == source) {
+		if ((cls[i][0].isActive == 0) || (cls[i] == source)) {
 			continue;
 		}
 
 		if (writeMessage(cls[i][0].fd, buf, count) < 0) {
-			cls[i][0].fd = -1; // disable the structure
+			cls[i][0].isActive = 0; // disable the structure
 		}
 
 	}
@@ -115,7 +186,7 @@ void cleanList() {
 
 	while(i < clN) {
 
-		if (cls[i][0].fd < 0) {
+		if (cls[i][0].isActive == 0) {
 
 			glbuf.time = time(NULL);		
 
@@ -123,6 +194,8 @@ void cleanList() {
 			sprintf(glbuf.message, "%s left.", cls[i][0].name);
 			
 			pthread_cancel(cls[i][0].thr);
+			close(cls[i][0].fd);
+			
 			removeClient(i);
 
 			broadcast(&glbuf, sizeof(struct buffer), NULL);
@@ -145,7 +218,7 @@ void * processClient(void *arg) {
 
 	int l; // length (of the string read)
 
-	// fisrt, register the client
+	// fisrt, identificate the client
 
 	sprintf(buf.name, "SERVER");
 	sprintf(buf.message, "Please, enter your name.");
@@ -164,24 +237,113 @@ void * processClient(void *arg) {
 	}
 	buf.message[l - 1] = 0;
 
-	// start initialisation
+	// start identification
 
-	block(); 
+	struct clientInfo *client; 
+
+	block(&lmutex); 
 
 	int i = 0;
-	while(heap[i].fd >= 0) { // search for an empty structure in the heap
+	while (heap[i].isActive) { // search for an empty structure in the heap
 		i++;
 	}
 
-	struct clientInfo *client = &heap[i];
+	client = &heap[i];
+
+	unblock(&lmutex);
 
 	client[0].fd = cfd;
 	sprintf(client[0].name, "%s", buf.message); 	
 	client[0].thr = pthread_self();
+	client[0].isActive = 1;
 
+	//check previous registration
+
+	i = regSeek(buf.message);
+
+	if (i < 0) { // then register as a new client
+
+		sprintf(buf.name, "SERVER");
+		sprintf(buf.message, "Please, enter a password.");
+		buf.time = time(NULL);
+		if (writeMessage(cfd, &buf, sizeof(struct buffer)) < 0) {
+			return NULL;
+		}
+
+		while (1) {
+
+			l = readMessage(cfd, buf.message, mesize);
+			if (l < 0) {
+				return NULL;
+			}
+			
+			if (l <= passLength) {
+				
+				buf.message[l - 1] = 0;
+
+				block(&rmutex);
+				regAdd(client[0].name, buf.message, l);
+				unblock(&rmutex);
+
+				break;
+
+			}
+
+			sprintf(buf.message, "The password is too long.");
+			buf.time = time(NULL);
+			if (writeMessage(cfd, &buf, sizeof(struct buffer)) < 0) {
+				return NULL;
+			}	
+			
+		}	
+
+	} else {
+
+		// then i == index of the password in the register
+
+		char *passw = &reg[i];
+
+		// inquire the password
+
+		sprintf(buf.name, "SERVER");
+		sprintf(buf.message, "Please, enter your password.");
+		buf.time = time(NULL);
+		if (writeMessage(cfd, &buf, sizeof(struct buffer)) < 0) {
+			client[0].isActive = 0;
+			return NULL;
+		}
+
+		while (1) {
+
+			l = readMessage(cfd, buf.message, mesize);
+			if (l < 0) {
+				client[0].isActive = 0;
+				return NULL;
+			}
+			
+			if (l <= passLength) {
+				
+				buf.message[l - 1] = 0;
+
+				if (strcmp(buf.message, passw) == 0) {
+					break;
+				}
+
+			}
+
+			sprintf(buf.message, "Incorrect password.");
+			buf.time = time(NULL);
+			if (writeMessage(cfd, &buf, sizeof(struct buffer)) < 0) {
+				return NULL;
+			}	
+			
+		}
+
+	}
+
+	block(&lmutex);
 	addClient(client); //add the *structure to the clients list
-
-	unblock();
+	unblock(&lmutex);
 
 	// the client registered
 
@@ -189,7 +351,7 @@ void * processClient(void *arg) {
 	buf.time = time(NULL);
 	sprintf(buf.message, "Welcome, %s!", client[0].name);
 	if (writeMessage(client[0].fd, &buf, sizeof(struct buffer)) < 0) {
-		client[0].fd = -1;
+		client[0].isActive = 0;
 		return NULL;
 	}
 
@@ -205,7 +367,7 @@ void * processClient(void *arg) {
 	
 		l = readMessage(client[0].fd, buf.message, mesize);
 		if (l < 0) {
-			client[0].fd = -1;
+			client[0].isActive = 0;
 			return NULL;
 		}
 
@@ -213,14 +375,14 @@ void * processClient(void *arg) {
 
 		buf.time = time(NULL);
 
-		block();
+		block(&lmutex);
 		
 		printf("Processing message from %s.\n", client[0].name);
 
 		broadcast(&buf, sizeof(struct buffer) - mesize + l, client);
 		cleanList();	
-	
-		unblock();
+
+		unblock(&lmutex);
 
 	}
 
@@ -240,7 +402,17 @@ void * waitCommand(void *arg) {
 		command[l - 1] = 0;
 
 		if (strcmp(command, "exit") == 0) {
-			
+		
+			int regfd = open("register", O_WRONLY);
+	
+			// copy reg into "register"
+
+			writeFull(regfd, reg, regSize);
+			check();
+
+			close(regfd);
+			check();
+
 			printf("Terminating.\n");
 
 			exit(0);
@@ -279,7 +451,7 @@ int main() {
 	// 2. initialise the clients list
 
 	for(clN = 0; clN < limit; clN++) {
-		heap[clN].fd = -1; // mark the structures as empty
+		heap[clN].isActive = 0; // mark the structures as empty
 	}
 
 	clN = 0; // clients number
@@ -288,7 +460,10 @@ int main() {
 
 	// 3. Prepare for multithread work
 	
-	errno = pthread_mutex_init(&mutex, NULL);
+	errno = pthread_mutex_init(&lmutex, NULL);
+	check();
+
+	errno =  pthread_mutex_init(&rmutex, NULL);
 	check();
 
 	pthread_t thr;
@@ -301,7 +476,47 @@ int main() {
 
 	// prepared for multithreading
 
-	// 4. Proceed to accept connections from clients
+	// 4. Load the clients register
+
+	/*
+
+	The structure of the register is the following:
+
+	<size of the unit><name><password><size of the unit><...
+
+	<size of the unit> has the type int and contains the
+	number of bytes between the end of itself
+	and the end of the unit
+
+	*/
+
+	int regfd = open("register", O_RDONLY | O_CREAT, S_IRWXU);
+	check();
+
+	#define unit 256
+
+	int l;
+
+	regSize = 0;
+
+	// copy the file "register" into the reg array
+	do {
+
+		l = read(regfd, &reg[regSize], unit);
+		check();
+
+		regSize += l;
+	
+	} while (l);
+
+	// the file copied
+
+	close(regfd);
+	check();
+
+	// the register is ready
+
+	// 5. Proceed to accept connections from clients
 
 	int cfd; // client file descriptor
 
